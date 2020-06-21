9016ad16e9cde9fe4204d2eca1c122d94edfd087
Andy Fischer <info@andyland.info>
2020-06-21 13:46:24 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'btouch_videomatrix adapter settings': {
+		'en': 'Adapter settings for btouch_videomatrix',
+		'de': 'Adaptereinstellungen für btouch_videomatrix',
+		'ru': 'Настройки адаптера для btouch_videomatrix',
+		'pt': 'Configurações do adaptador para btouch_videomatrix',
+		'nl': 'Adapterinstellingen voor btouch_videomatrix',
+		'fr': "Paramètres d'adaptateur pour btouch_videomatrix",
+		'it': "Impostazioni dell'adattatore per btouch_videomatrix",
+		'es': 'Ajustes del adaptador para btouch_videomatrix',
+		'pl': 'Ustawienia adaptera dla btouch_videomatrix',
+		'zh-cn': 'btouch_videomatrix的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
