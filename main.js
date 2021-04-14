01d9e9eb6b2056ef2f56e907966616c088fc46cb
Andy Fischer <info@andyland.info>
2021-04-14 18:49:10 +0200

    refactor

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ class BtouchVideomatrix extends utils.Adapter {
 		// Kombinatinen von Ein- und Ausgang als Nummer ('Eingang 1 auf X')
 		//for (var i = 0; i < MAXCHANNELS; i++) {
 		//	for (var j = 0; j < MAXCHANNELS; j++) {
-		await this.setObjectAsync('ASD/test_input_' + (i + 1).toString().padStart(2, '0') + '_out_' + (j + 1).toString().padStart(2, '0'), {
+		await this.setObjectAsync('ASD.test_input_' + (i + 1).toString().padStart(2, '0') + '_out_' + (j + 1).toString().padStart(2, '0'), {
 			type: 'state',
 			common: {
 				name: 'Connect Input to numbered Output',
@@ -143,7 +143,7 @@ class BtouchVideomatrix extends utils.Adapter {
 				states: { 0: 'Off', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6' },
 				role: 'list',
 				read: true,
-				write: false,
+				write: true,
 			},
 			native: {},
 		});
