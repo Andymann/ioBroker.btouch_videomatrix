303f62c5ba52ddafab4ea4e2d6f453dddd2d7a8a
Andy Fischer <info@andyland.info>
2021-04-17 20:46:18 +0200

    refactor

--- main.js
+++ main.js
@@ -188,7 +188,8 @@ class BtouchVideomatrix extends utils.Adapter {
 		var wert = await this.getStateAsync(id);
 		this.log.info('readLabels():' + wert.val);
 
-		this.log.info('readLabels() 2:' + await this.getStateAsync('Labels.input_' + (1).toString().padStart(2, '0')).val);
+		var wert2 = await this.getStateAsync('Labels.input_' + (1).toString().padStart(2, '0')).val;
+		this.log.info('readLabels() 2:' + wert2);
 	}
 
 
