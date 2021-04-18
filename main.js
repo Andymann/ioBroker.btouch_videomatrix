54a05ae0cdc1b0fb25596a5ccdcd2963b49ab234
Andy Fischer <info@andyland.info>
2021-04-18 12:45:03 +0200

    refactor

--- main.js
+++ main.js
@@ -193,13 +193,13 @@ class BtouchVideomatrix extends utils.Adapter {
 		//var wert = await this.getStateAsync(id);
 		this.log.info('readLabels():');
 
-		var wert2 = await this.getStateAsync('Labels.input_' + (1).toString().padStart(2, '0'));
-		var wert3 = await this.getStateAsync('Labels.output_' + (1).toString().padStart(2, '0'));
-		this.log.info('readLabels() 2:' + wert2.val + ' ' + wert3.val);
+		//var wert2 = await this.getStateAsync('Labels.input_' + (1).toString().padStart(2, '0'));
+		//var wert3 = await this.getStateAsync('Labels.output_' + (1).toString().padStart(2, '0'));
+		//this.log.info('readLabels() 2:' + wert2.val + ' ' + wert3.val);
 
 		for (var i = 0; i < parentThis.MAXCHANNELS; i++) {
-			var tmpIn = await this.getStateAsync('Labels.input_' + (i).toString().padStart(2, '0'));
-			var tmpOut = await this.getStateAsync('Labels.output_' + (i).toString().padStart(2, '0'));
+			var tmpIn = await this.getStateAsync('Labels.input_' + (i + 1).toString().padStart(2, '0'));
+			var tmpOut = await this.getStateAsync('Labels.output_' + (i + 1).toString().padStart(2, '0'));
 
 			//var elementIn = { i: tmpIn.val };
 			//var elementOut = { i: tmpOut.val };
