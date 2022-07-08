709bb6419e9b5d0e0da0f0b89f076ff6ad5703d9
Andy Fischer <info@andyland.info>
2022-07-08 13:22:12 +0200

    handle switching OFF of output

--- main.js
+++ main.js
@@ -864,8 +864,8 @@ class BtouchVideomatrix extends utils.Adapter {
 			const tmpOUT = sMSG.substring(iStart, sMSG.indexOf(' '));
 			parentThis.log.debug('parseMSG(): OFF:' + tmpOUT);
 			for (let i = 0; i < parentThis.MAXCHANNELS; i++) {
-				//this.setStateAsync('input_' + (i + 1).toString().padStart(2, '0') + '_out_' + (tmpOUT.trim().padStart(2, '0'), { val: false, ack: true }));
-				this.log.info('input_' + (i + 1).toString().padStart(2, '0') + '_out_' + tmpOUT.trim().padStart(2, '0'));
+				this.setStateAsync('input_' + (i + 1).toString().padStart(2, '0') + '_out_' + (tmpOUT.trim().padStart(2, '0'), { val: false, ack: true }));
+				//this.log.info('input_' + (i + 1).toString().padStart(2, '0') + '_out_' + tmpOUT.trim().padStart(2, '0'));
 			}
 
 		} else if (sMSG.toLowerCase().includes('all.')) {
